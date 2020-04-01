05d2c47bf822f7e2033c19031cb50ce85d3352bd
Carl Churchill <61422819+whrcdontfearme22@users.noreply.github.com>
2020-04-01 10:22:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ var followPoint = true;
 
 // ...If so, what zoom, pitch, and bearing should be used to follow?
 // Zoomlevel lower = zoomed out
-var followZoomLevel = 7;
+var followZoomLevel = 4;
 var followBearing = -47.20;
 var followPitch = 0;
 
